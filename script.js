4459bf20b1ad40cf628565f8001343d8c017b838
clive_mb_air <clivetoniato1@hotmail.com>
2026-03-23 20:43:08 +0000

    unary - started

--- script.js
+++ script.js
@@ -26,24 +26,34 @@ function tokeniser(expr) {
     } 
     
     else if (/ /.test(expr[i])) {
-      tokens.push(currToken.join(""));
-      currToken.length = 0;
-    } 
-    // operations.has(expr[i])
-    else if (/[+-/*]/.test(expr[i])) {
     if(currToken.length){
 
         tokens.push(currToken.join(""));
         currToken.length = 0;
     }
-      currToken.push(expr[i]);
-      tokens.push(currToken.join(""));
-      currToken.length = 0;
+    } 
+    // operations.has(expr[i])
+    else if (/[+-/*]/.test(expr[i])) {
+    if(expr[i] == '-' && tokens.length==0 || /[+-/*(]/.test(tokens[tokens.length-1])){
+        currToken.push(expr[i])
+    }
+    else if(currToken.length){
+
+        tokens.push(currToken.join(""));
+        currToken.length = 0;
+    }
+    else{
+
+        currToken.push(expr[i]);
+        tokens.push(currToken.join(""));
+        currToken.length = 0;
+    }
     }
   }
   return tokens;
 }
-
+ console.log(tokeniser("-2 -(-5)"))
+ console.log(tokeniser("4 * -2"))
 // console.log(tokeniser('4 - 2'))
 console.log(tokeniser("+"));
- console.log(tokeniser('41 + ( 98+9) - 2'))
+//  console.log(tokeniser('41 + ( 98+9) - 2'))
