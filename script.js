1c545b213c7dc891d8bb6dae97d4d580fc4bf85a
clive_mb_air <clivetoniato1@hotmail.com>
2026-03-23 15:25:14 +0000

    first tokeniser

---
+++ script.js
@@ -0,0 +1,49 @@
+// collect more than one digit
+// collect operators
+// ignore space
+// collect parantheses
+// 4 - 2
+function tokeniser(expr) {
+  operations = new Set("-", "+", "/", "*");
+  
+  expr = expr + " ";
+  let tokens = [];
+  let currToken = [];
+  for (let i = 0; i < expr.length; i++) {
+    if (expr[i] == "(" || expr[i] == ")") {
+      if(currToken.length){
+
+        tokens.push(currToken.join(""));
+        currToken.length = 0;
+    }
+      currToken.push(expr[i]);
+      tokens.push(currToken.join(""));
+       currToken.length = 0;
+    }
+    
+    else if (/\d/.test(expr[i])) {
+      currToken.push(expr[i]);
+    } 
+    
+    else if (/ /.test(expr[i])) {
+      tokens.push(currToken.join(""));
+      currToken.length = 0;
+    } 
+    // operations.has(expr[i])
+    else if (/[+-/*]/.test(expr[i])) {
+    if(currToken.length){
+
+        tokens.push(currToken.join(""));
+        currToken.length = 0;
+    }
+      currToken.push(expr[i]);
+      tokens.push(currToken.join(""));
+      currToken.length = 0;
+    }
+  }
+  return tokens;
+}
+
+// console.log(tokeniser('4 - 2'))
+console.log(tokeniser("+"));
+ console.log(tokeniser('41 + ( 98+9) - 2'))
